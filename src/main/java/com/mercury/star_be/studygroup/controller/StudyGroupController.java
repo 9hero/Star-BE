3fc13ef19ddb9f3c1386a91eaa36314709c1ea4f
“mafatofu” <qwqw0310@gmail.com>
2025-01-16 15:46:50 +0900

    infra : backend와 frontend 데이터 통신 셋팅 WebConfig의 WebMvcConfigurer - addCorsMappings의 allowedOrigins부분은 현재 localhost:5173으로 연결되어있음. 추후 서버에 올릴 때 변경해야 함

--- src/main/java/com/mercury/star_be/studygroup/controller/StudyGroupController.java
+++ src/main/java/com/mercury/star_be/studygroup/controller/StudyGroupController.java
@@ -1,5 +1,7 @@
 package com.mercury.star_be.studygroup.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -7,4 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class StudyGroupController {
+    @GetMapping("/api/testData")
+    public ResponseEntity<String> testData() {
+        return ResponseEntity.ok("Hello front!! im backend Data~");
+    }
 }
