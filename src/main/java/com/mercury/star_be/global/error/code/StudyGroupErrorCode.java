63c6acd45393ad859f963ec42e111cc0963079d4
jsjseop <57559288+jsjseop@users.noreply.github.com>
2025-01-17 14:19:42 +0900

    refactor: ErrorCode 수정

--- src/main/java/com/mercury/star_be/global/error/code/StudyGroupErrorCode.java
+++ src/main/java/com/mercury/star_be/global/error/code/StudyGroupErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum StudyGroupErrorCode {
+public enum StudyGroupErrorCode implements ErrorCode {
 
 	// 스터디 그룹 관련 에러
 	STUDY_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 스터디 그룹입니다.");
