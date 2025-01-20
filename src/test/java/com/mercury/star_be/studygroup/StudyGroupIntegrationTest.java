299f4db6147218e33087346550c45a02fbff35ee
jsjseop <57559288+jsjseop@users.noreply.github.com>
2025-01-20 15:11:27 +0900

    test: RestDocs 적용

--- src/test/java/com/mercury/star_be/studygroup/StudyGroupIntegrationTest.java
+++ src/test/java/com/mercury/star_be/studygroup/StudyGroupIntegrationTest.java
@@ -1,36 +1,16 @@
 package com.mercury.star_be.studygroup;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercury.star_be.common.RestDocsTestSupport;
 import com.mercury.star_be.studygroup.dto.request.StudyGroupCreateRequest;
 
-@SpringBootTest
-@Testcontainers
-@Transactional
-@ActiveProfiles("test")
-@AutoConfigureMockMvc
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class StudyGroupIntegrationTest {
-
-	@Autowired
-	MockMvc mockMvc;
-	@Autowired
-	ObjectMapper objectMapper;
+class StudyGroupIntegrationTest extends RestDocsTestSupport {
 
 	@Test
 	@DisplayName("스터디 그룹을 생성한다.")
@@ -54,6 +34,6 @@ class StudyGroupIntegrationTest {
 				.accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.data.id").value(1))
-			.andDo(print());
+			.andDo(restDocs.document());
 	}
 }
