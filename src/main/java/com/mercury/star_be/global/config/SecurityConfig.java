d5e520760a69f92bf27f82df7a9fd154eeeaea69
“mafatofu” <qwqw0310@gmail.com>
2025-01-27 14:49:56 +0900

    style : chat 웹소켓 url 허용

--- src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
+++ src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
@@ -15,7 +15,8 @@ public class SecurityConfig{
                         .requestMatchers(
                                 "/api/**" // front단에서의 요청
                                 , "/docs/index.html" // rest docs 요청
-                                ,"/timer/**" // timer 웹소켓
+                                ,"/timer/**", // timer 웹소켓
+                                "/chat/**" // chat 웹소켓
                         ).permitAll() //기본 permiAll로 셋팅. 추후 변경 필요
                         .anyRequest().authenticated()  // 위 경로 말고 다른 경로들은 전부 인증필요
                 );
