44ade2b9bff0b8fad9288a79f62ba38615d504e0
“mafatofu” <qwqw0310@gmail.com>
2025-01-30 13:52:17 +0900

    feat : 채팅 에러 코드 추가

--- src/main/java/com/mercury/star_be/global/error/code/ChatErrorCode.java
+++ src/main/java/com/mercury/star_be/global/error/code/ChatErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ChatErrorCode implements ErrorCode {
     CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다."),
-    CHAT_MESSAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "채팅메시지를 찾을 수 없습니다.");
+    CHAT_MESSAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "채팅메시지를 찾을 수 없습니다."),
+    USER_CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 채팅방을 찾을 수 없습니다."),
+    CHAT_MESSAGE_CONVERT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "메시지 변환이 올바르게 적용되지 않았습니다."),
+    MESSAGE_SENDING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "RabbitMQ로 메시지를 전달하지 못했습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
