b0cab1b7b0cd73bca7c7dbec109f9c1fd213218d
Laon0125 <87473656+Laon0125@users.noreply.github.com>
2025-02-10 13:24:05 +0900

    Update SecurityConfig.java

--- src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
+++ src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
@@ -58,8 +58,8 @@ public class SecurityConfig{
                 CorsConfiguration configuration = new CorsConfiguration();
 
                 configuration.setAllowedOrigins(Arrays.asList(
-                    "http://34.22.66.212:3001", 
-                    "http://34.22.98.26:8080", 
+                    "https://34.22.66.212:3001", 
+                    "https://34.22.98.26:8080", 
                     "http://mercurystarback.duckdns.org", 
                     "https://mercurystar.duckdns.org"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
