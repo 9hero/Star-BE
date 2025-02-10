2efefaeb9a34a24dd69f691d074d80133c53deb5
9hero <rngnlah@naver.com>
2025-02-10 16:36:27 +0900

    feat: 주간, 일간, 월간 랭킹 조회 api.
    native 쿼리로 작성했습니다. 전체 레코드를 조회해옵니다.

--- src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
+++ src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig{
                                 ,"/", "/login/oauth2/code/**", "/oauth2-jwt-header", "/oauth2Login", "/api/check-auth", "/oauth2/callback",
                                 "/api/auth/reissue", "/api/groups/**", "/groups/**", "/error","/reissue",
                                 "/groups",
-                                "/fileupload/**"
+                                "/fileupload/**",
+                                "/api/timers/ranking/**"
                         ).permitAll() //기본 permiAll로 셋팅. 추후 변경 필요
                         .anyRequest().authenticated()  // 위 경로 말고 다른 경로들은 전부 인증필요
                 );
---
+++ src/main/java/com/mercury/star_be/timer/dto/TimerRankingPeriod.java
@@ -0,0 +1,5 @@
+package com.mercury.star_be.timer.dto;
+
+public enum TimerRankingPeriod {
+    DAILY, WEEKLY, MONTHLY
+}
