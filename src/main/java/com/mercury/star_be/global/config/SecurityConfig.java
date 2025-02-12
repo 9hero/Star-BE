f69e178d20db1408a6e0e762d68384e23645491e
jsjseop <57559288+jsjseop@users.noreply.github.com>
2025-02-12 12:40:24 +0900

    fix: 그룹 탈퇴 시 사용자 채팅방 삭제

--- src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
+++ src/main/java/com/mercury/star_be/global/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig{
                     // "https://34.22.98.26:8080",
                     "http://localhost:5173",
                     "https://mercurystudy.store"));
-                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173", "https://mercurystudy.store"));
                 configuration.setAllowedHeaders(List.of("*"));
                 configuration.setAllowCredentials(true);
